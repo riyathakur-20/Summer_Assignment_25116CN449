570e1cbf295deca350f5c097f445775918b79728
riyathakur-20 <riya.thakur1785@gmail.com>
2026-06-02 22:59:04 +0530

    Added Day 1 and Day 2 programs

--- day2_prgrm1.cpp
+++ day2_prgrm1.cpp
@@ -1,21 +1,23 @@
-//Write a program to Count digits in a number. 
+//Write a program to Find sum of digits of a number. 
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int num, count = 0;
+    int num, digit, sum = 0;
 
     cout << "Enter a number: ";
     cin >> num;
 
     while(num != 0)
     {
+        digit = num % 10;
+        sum = sum + digit;
         num = num / 10;
-        count++;
     }
 
-    cout << "Total digits = " << count;
+    cout << "Sum of digits = " << sum;
 
     return 0;
 }
---
+++ day2_prgrm2.cpp
@@ -0,0 +1,23 @@
+//Write a program to Reverse a number. 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, reverse = 0, digit;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while(num != 0)
+    {
+        digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = num / 10;
+    }
+
+    cout << "Reversed number = " << reverse;
+
+    return 0;
+}
