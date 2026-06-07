2a8b6ffbc217ad36a6885bf7f0c8d40b98f75ca0
riyathakur-20 <riya.thakur1785@gmail.com>
2026-06-07 22:17:45 +0530

    Added day 7 programs

---
+++ day7_prgrm2.cpp
@@ -0,0 +1,25 @@
+//Write a program to Recursive Fibonacci. 
+
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter position: ";
+    cin >> n;
+
+    cout << "Fibonacci term = " << fibonacci(n);
+
+    return 0;
+}
