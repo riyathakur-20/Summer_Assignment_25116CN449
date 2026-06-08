5863f4860ead3dac690b9136f0c3f1931c5c84d3
riyathakur-20 <riya.thakur1785@gmail.com>
2026-06-08 21:49:28 +0530

    Added day 8 programs.

---
+++ day8_prgrm4.cpp
@@ -0,0 +1,17 @@
+//Write a program to Print character triangle.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for(int i = 1; i <= n; i++) {
+        for(char ch = 'A'; ch < 'A' + i; ch++) {
+            cout << ch;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
