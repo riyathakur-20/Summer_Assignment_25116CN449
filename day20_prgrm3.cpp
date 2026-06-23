b08b6d318809fbb01332e5658f5d8211a00ef6d3
riyathakur-20 <riya.thakur1785@gmail.com>
2026-06-23 19:42:49 +0530

    day 20 added

---
+++ day20_prgrm3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int r, c;
+
+    cout << "Enter rows and columns: ";
+    cin >> r >> c;
+
+    int a[r][c];
+
+    cout << "Enter matrix elements:\n";
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            cin >> a[i][j];
+
+    cout << "Row-wise Sum:\n";
+
+    for(int i = 0; i < r; i++)
+    {
+        int sum = 0;
+
+        for(int j = 0; j < c; j++)
+            sum += a[i][j];
+
+        cout << "Row " << i + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}
